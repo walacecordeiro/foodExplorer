d8d9d86278b7af5b46c9bdc96523c422075e4b60
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-09-26 16:47:36 -0300

    usei route params
    alterei a rota para que ela possa capturar parâmetros(valores) na URL. Quando uma solicitação é feita para essa rota, é enviada uma resposta com os valores dos parâmetros

--- serverSide/src/server.js
+++ serverSide/src/server.js
@@ -4,10 +4,16 @@ const express = require("express");
 // Inicializa o aplicativo Express.
 const app = express();
 
-// Define uma rota para a raiz do servidor (URL "/") que responde a solicitações GET.
-app.get("/", (request, response) => {
-  // Quando uma solicitação GET é feita para a raiz, envia uma resposta com o texto especificado.
-  response.send("Esse é o caminho para a raiz do servidor");
+// Define uma rota que captura parâmetros na URL, como "/pedidos/123/produto".
+app.get("/pedidos/:id/:item", (request, response) => {
+  // Extrai os valores dos parâmetros da URL usando 'request.params'.
+  const { id, item } = request.params;
+
+  // Quando uma solicitação GET é feita para essa rota, envia uma resposta com os valores dos parâmetros.
+  response.send(`
+    Número do pedido: ${id}
+    Item pedido: ${item}
+  `);
 });
 
 // Define a porta em que o servidor irá escutar as solicitações HTTP.
