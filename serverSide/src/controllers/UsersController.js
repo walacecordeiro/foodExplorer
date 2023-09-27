b7ffabc5c382c75be54fc27e179d5a42110732bc
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-09-27 15:22:14 -0300

    Adicionei um Middleware no 'server.js' para fazer o tratamento de erros personalizados
    tratamento de exceções; erros personalizados

--- serverSide/src/controllers/UsersController.js
+++ serverSide/src/controllers/UsersController.js
@@ -1,9 +1,18 @@
+// Importa a classe 'AppError' que permite criar erros personalizados.
+const AppError = require("../utils/AppError");
+
 // Define uma classe chamada UsersController para gerenciar operações relacionadas a usuários.
 class UsersController {
   // Define um método 'create' que lida com a criação de usuários.
-  create(request, response){
+  create(request, response) {
     // Extrai os dados do corpo da solicitação JSON usando 'request.body'.
     const { name, email, password } = request.body;
+
+    // Verifica se o campo 'name' não está vazio.
+    if (!name) {
+      // Se o campo 'name' estiver vazio, lança um erro personalizado usando a classe 'AppError'.
+      throw new AppError("Nome é obrigatório");
+    }
     // Envia uma resposta JSON com os dados rebebidos no corpo da solicitação e define o código de status HTTP como 201 (created).
     response.status(201).json({ name, email, password });
   }
--- serverSide/src/server.js
+++ serverSide/src/server.js
@@ -1,3 +1,9 @@
+// Importa o módulo 'express-async-errors' para lidar com erros assíncronos automaticamente.
+require("express-async-errors");
+
+// Importa a classe 'AppError' que permite criar erros personalizados.
+const AppError = require("./utils/AppError");
+
 // Importa o módulo 'express' para criar um servidor web.
 const express = require("express");
 
@@ -12,6 +18,25 @@ app.use(express.json());
 
 // Adiciona as rotas definidas no módulo 'routes' ao aplicativo.
 app.use(routes);
+
+// Middlware de tratamento de erros personalizados.
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    // Se o erro for uma instância de 'AppError', trata-o como um erro conhecido e retorna o 'AppError'.
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.error(error)
+
+  // Se o erro não for uma instância de 'AppError', trata-o como erro interno de servidor.
+  return response.status(500).json({
+    status: "error",
+    message: "Erro interno no servidor",
+  });
+});
 
 // Define a porta em que o servidor irá escutar as solicitações HTTP.
 const PORT = 3333;
