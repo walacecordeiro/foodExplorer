3bda7be9350ae27e52c6beb5081c5d8dcc3efa5a
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-09-29 18:15:49 -0300

    adicinei lógica de atualização de senha do usuário
    no método update do 'UsersController.js' adicionei a lógica para verificar a senha atual e atualizar caso o usuário forneça os dados da própria e da senha nova

--- serverSide/src/controllers/UsersController.js
+++ serverSide/src/controllers/UsersController.js
@@ -1,5 +1,5 @@
 // Importa a função 'hash' do módulo 'bcryptjs' para criptografar dados sensíveis.
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 
 // Importa a classe 'AppError' que permite criar erros personalizados.
 const AppError = require("../utils/AppError");
@@ -48,8 +48,8 @@ class UsersController {
 
   // Define um método 'update' que lida com a atualização de informações de usuários.
   async update(request, response) {
-    // Extrai os dados do corpo da solicitação JSON, incluindo 'name' e 'email'.
-    const { name, email } = request.body;
+    // Extrai os dados do corpo da solicitação JSON, incluindo 'name', 'email', 'password' e 'old_password'.
+    const { name, email, password, old_password } = request.body;
 
     // Extrai o ID do usuário a ser atualizado dos parâmetros da URL.
     const { id } = request.params;
@@ -79,6 +79,29 @@ class UsersController {
     // Atualiza os campos 'name' e 'email' do usuário com os novos valores.
     user.name = name;
     user.email = email;
+    
+    // Verifica se uma nova senha (password) foi fornecida, mas a senha antiga (old_password) não foi fornecida.
+    if (password && !old_password) {
+      // Se a senha antiga não foi fornecida, lança um erro personalizado indicando que ela é necessária para definir uma nova senha.
+      throw new AppError(
+        "Você precisa informar a senha antiga para definir a nova senha"
+      );
+    }
+
+    // Verifica se tanto a nova senha quando a senha antiga foram fornecidas.
+    if (password && old_password) {
+      // Verifica se a senha antiga (old_password) corresponde à senha atual do usuário no banco de dados.
+      const checkOldPassword = await compare(old_password, user.password);
+
+      // Se a senha antiga não corresponder à senha atual, lança um erro personalizado.
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga não confere.");
+      }
+
+      // Se a senha antiga corresponder à senha atual, prossegue com a atualização da senha.
+      // A nova senha é criptografada usando 'hash' do módulo 'bcryptjs' com um fator de custo 8.
+      user.password = await hash(password, 8);
+    }
 
     // Executa uma sonsulta SQL para atualizar os dados do usuário no banco de dados, incluindo a atualização do campo 'updated_at'.
     await database.run(
@@ -86,11 +109,12 @@ class UsersController {
       UPDATE users SET
       name = ?,
       email = ?,
+      password = ?,
       updated_at = ?
       WHERE id = ?`,
-      [user.name, user.email, new Date(), id]
+      [user.name, user.email, user.password, new Date(), id]
     );
-    
+
     // Retorna uma resposta JSON vazia para indicar que a operação de atualização foi bem-sucedida.
     return response.json();
   }
