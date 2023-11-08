ebb89abd4e999131d28ca46806c67075cc221258
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-11-08 17:49:33 -0300

    carragar imagens para pasta temporária no backend

--- serverSide/src/routes/dishes.routes.js
+++ serverSide/src/routes/dishes.routes.js
@@ -1,29 +1,24 @@
-// Importa o módulo 'Router' do 'Express' para cirar um roteador.
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
-// Importa o controlador 'DishesController.js'. que é uma classe responsável por gerenciar as operações relacionadas aos pratos.
 const DishesController = require("../controllers/DishesController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
-// Cria um novo roteador chamado 'dishesRoutes' específico para as rotas de pratos.
 const dishesRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
-// Cria uma instância do controlador 'DishesController.js' para lidar com as operações relacionadas aos pratos.
 const dishesController = new DishesController();
 
 dishesRoutes.use(ensureAuthenticated);
 
-// Define uma rota GET ("/") que delega o controle ao método 'index' do controlador 'DishesController.js'.
 dishesRoutes.get("/", dishesController.index);
-
-// Define uma rota POST ("/:user_id") que delega o controle ao método 'create' do controlador 'DishesController.js'.
 dishesRoutes.post("/", dishesController.create);
-
-// Define uma rota GET ("/:id") que delega o controle ao método 'show' do controlador 'DishesController.js'.
 dishesRoutes.get("/:id", dishesController.show);
-
-// Derfine uma rota DELETE ("/:id") que delega o controle ao método 'delete' do controlador 'DishesController.js'.
 dishesRoutes.delete("/:id", dishesController.delete);
+dishesRoutes.patch("/image", upload.single("image"), (request, response) => {
+  console.log(request.file.filename);
+  response.json();
+});
 
-// Exporta o roteador 'dishesRoutes' para ser utilizado em outros lugares do aplicativo.
 module.exports = dishesRoutes;
