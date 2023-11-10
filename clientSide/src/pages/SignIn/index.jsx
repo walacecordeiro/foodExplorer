cb931232e6fde0712d52f2a444921a9d34cff727
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-11-10 16:26:57 -0300

    função de autenticação funcionando

--- clientSide/src/pages/SignIn/index.jsx
+++ clientSide/src/pages/SignIn/index.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { useAuth } from "../../hooks/auth";
 
 import { Container } from "./styles";
@@ -9,19 +10,33 @@ import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
 
 export function SignIn() {
-  const data = useAuth();
-  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password});
+  }
+
   return (
     <Container>
       <Logo $width={43} $height={43} />
       <Form>
         <label htmlFor="email">Email</label>
-        <Input id="email" placeholder="Exemplo: exemplo@exemplo.com" />
+        <Input
+          id="email"
+          placeholder="Exemplo: exemplo@exemplo.com"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label htmlFor="password">Senha</label>
-        <Input id="password" placeholder="No mínimo 6 caracteres" />
+        <Input
+          id="password"
+          placeholder="No mínimo 6 caracteres"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Button label="Entrar" />
+        <Button label="Entrar" onClick={handleSignIn}/>
       </Form>
       <Link to="/cadastrar">Criar uma conta</Link>
     </Container>
