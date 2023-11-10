b4d6dd49b43d7b8a460aa18fbdad7fcca17886af
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-11-10 16:32:03 -0300

    redirecionando a rotas do usuário depois de logar

--- clientSide/src/routes/index.jsx
+++ clientSide/src/routes/index.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter } from "react-router-dom";
+import { useAuth } from "../hooks/auth";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
-export function Routes(){
+export function Routes() {
+  const { user } = useAuth();
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      {user ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
