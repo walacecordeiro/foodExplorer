6bb442c20d07ebdcf6f6a240df1b4892d190c18b
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-11-10 16:40:47 -0300

    utilizei o localStorage para manter o usuário no estado logado, nas rotas de usuário logado

--- clientSide/src/hooks/auth.jsx
+++ clientSide/src/hooks/auth.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
@@ -13,6 +13,9 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
 
+      localStorage.setItem("@foodexplorer:user", JSON.stringify(user));
+      localStorage.setItem("@foodexplorer:token", token);
+
       api.defaults.headers.authorization = `Bearer ${token}`;
       setData({ user, token });
     } catch (error) {
@@ -24,6 +27,19 @@ function AuthProvider({ children }) {
     }
   }
 
+  useEffect(() => {
+    const token = localStorage.getItem("@foodexplorer:token");
+    const user = localStorage.getItem("@foodexplorer:user");
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+  }, []);
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
       {children}
